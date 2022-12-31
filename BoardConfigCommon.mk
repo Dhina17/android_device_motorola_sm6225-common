a9f522acf79ee728f5d9fd1bf521acd085dba2a2
Dhina17 <dhinalogu@gmail.com>
2022-12-31 11:16:06 +0530

    sm6225-common: Configure boot/kernel
    Change-Id: I0fa52ee9cd23bfcf85fe14b5986e6f7f1e0f103d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,5 +23,19 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 TARGET_BOOTLOADER_BOARD_NAME := bengal
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+TARGET_KERNEL_CONFIG := vendor/bengal_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/sm6225
+
+# Kernel - Prebuilt
+include device/motorola/sm6225-kernel/prebuilt_kernel.mk
+
 # Platform
 TARGET_BOARD_PLATFORM := bengal
