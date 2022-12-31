f7e31708543967b5055c0daf89df77d9b7f7ef28
Dhina17 <dhinalogu@gmail.com>
2022-12-31 10:54:46 +0530

    sm6225-common: Add bootloader flags
    Change-Id: I6c3fcddb73db3e43327b68df3652230ad1cbe04b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,5 +19,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := bengal
+TARGET_NO_BOOTLOADER := true
+
 # Platform
 TARGET_BOARD_PLATFORM := bengal
